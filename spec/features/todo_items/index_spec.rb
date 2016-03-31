507e13ed7cd242da2a5cb053e2a7b8b3ddacf0c7
Josh Tatterton <joshua.tatterton@matalandirect.com>
2016-03-31 12:52:39 +0100

    Improve visual for todo list index & show page
    Some of the scaffold tests have been removed due to being pending and
    clogging up the rspec results.

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -14,7 +14,7 @@ describe "Viewing todo items" do
     todo_list.todo_items.create(content: "Eggs")
     visit_todo_list(todo_list)
 
-    expect(page.all("table.todo_items tr").size).to eq(2)
+    expect(page.all("table.todo_items tbody tr").size).to eq(2)
 
     within "table.todo_items" do
       expect(page).to have_content("Milk")
