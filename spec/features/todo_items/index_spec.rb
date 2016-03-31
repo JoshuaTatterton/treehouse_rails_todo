ca10e1524b8d50b2c37ae805b2ab9d35cc5154c1
Josh Tatterton <joshua.tatterton@matalandirect.com>
2016-03-31 09:30:58 +0100

    Add ability to create todo items
    "--require spec_helper" was added to the .rspec file so these lines are
    now no longer needed in the individual spec files.

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -1,4 +1,3 @@
-require "spec_helper"
 
 describe "Viewing todo items" do
   let!(:todo_list) { TodoList.create(title: "Shopping", description: "shopping")}
--- spec/features/todo_lists/destroy_spec.rb
+++ spec/features/todo_lists/destroy_spec.rb
@@ -1,4 +1,3 @@
-require "spec_helper"
 
 describe "Deleting todo lists" do
 
